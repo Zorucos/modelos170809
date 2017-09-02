e3cb72cca62fd97789a824dc4a83b9f283399015
ismaelsoruco <30440769+ismaelsoruco@users.noreply.github.com>
2017-09-02 13:48:00 +0200

    heroku
    traying heroku

--- WG/settings/email_info.py
+++ WG/settings/email_info.py
@@ -3,5 +3,5 @@ import os
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'wgmodelos@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_HOST_PASSWORD = 'smile098' #os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
